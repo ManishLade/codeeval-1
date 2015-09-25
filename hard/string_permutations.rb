c0190a8e07fa9ac95fa5d7173a32260c593729f0
bdauria <bruno.dauria1@gmail.com>
2015-09-25 21:40:49 +0200

    add commentars to string_permutations

--- hard/string_permutations.rb
+++ hard/string_permutations.rb
@@ -1,3 +1,8 @@
+# Write a program which prints all the permutations 
+# of a string in alphabetical order. We consider that 
+# digits < upper case letters < lower case letters.
+# The sorting should be performed in ascending order.
+
 lines = File.open(ARGV[0])
 lines.each_line do |line|
   chomped = line.chomp
